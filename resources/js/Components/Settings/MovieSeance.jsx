ce0bf7c8a66a3bf57056b391430771587165f0cf
ustasnov <ustasnov@gmail.com>
2024-12-25 00:10:51 +0700

    Changes for MovieSeance component

--- resources/js/Components/Settings/MovieSeance.jsx
+++ resources/js/Components/Settings/MovieSeance.jsx
@@ -28,14 +28,71 @@ export default function MovieSeance({ data }) {
         seanceEl.style.backgroundColor = getFilmColor(data.film_id);
     }, [data]);
 
+    function closeAllOtherMenu(el) {
+        const dropdowns = document.getElementsByClassName('dropdown-content');
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown !== el) {
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+    function closeMenu(ev) {
+        const parentEl = ev.currentTarget.closest('.dropdown-content');
+        if (parentEl) {
+            if (parentEl.classList.contains('show')) {
+                parentEl.classList.remove('show');
+            }
+        }
+    }
+
+    function clickHandler(ev) {
+        const dropdownEl = ev.currentTarget.querySelector('.dropdown-content');
+        if (dropdownEl) {
+            closeAllOtherMenu(dropdownEl);
+            dropdownEl.classList.toggle('show');
+        }
+        ev.stopPropagation();
+    }
+
+    function editHandler(ev) {
+        closeMenu(ev);
+        ev.preventDefault();
+        ev.stopPropagation();
+        //showUpdateModal();
+    }
+
+    function deleteHandler(ev) {
+        closeMenu(ev);
+        ev.preventDefault();
+        ev.stopPropagation();
+        //showDeleteModal();
+    }
+
     return (
         <div
             className="conf-step__seances-movie"
             data-id={data.id}
             style={{ width: width, left: start_offset }}
+            onClick={clickHandler}
         >
-            <p className="conf-step__seances-movie-title">{data.film_name}</p>
-            <p className="conf-step__seances-movie-start">{timeStr}</p>
+            <div className="dropdown">
+                <p className="conf-step__seances-movie-title">
+                    {data.film_name}
+                </p>
+                <p className="conf-step__seances-movie-start">{timeStr}</p>
+                <div className="dropdown-content">
+                    <a href="#" onClick={editHandler}>
+                        Редактировать
+                    </a>
+                    <a href="#" onClick={deleteHandler}>
+                        Удалить
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
